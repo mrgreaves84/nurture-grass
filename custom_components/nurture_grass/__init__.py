2f8be82ab757a4ab5d550f2aa0cbe473f5a98928
mrgreaves84 <140209089+mrgreaves84@users.noreply.github.com>
2026-06-15 05:28:11 +0100

    Add Nurture Grass Schedule integration source files

--- custom_components/nurture_grass/__init__.py
+++ custom_components/nurture_grass/__init__.py
@@ -1,1 +1,68 @@
+"""Nurture Grass Schedule."""
+import logging
+_LOGGER = logging.getLogger(__name__)
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+from .coordinator import NurtureCoordinator
+
+PLATFORMS = [
+    "sensor",
+    "binary_sensor",
+    "button",
+    "calendar",
+]
+
+
+async def async_setup(
+    hass: HomeAssistant,
+    config: dict,
+) -> bool:
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+
+    coordinator = NurtureCoordinator(
+        hass,
+        entry,
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(
+        DOMAIN,
+        {}
+    )[entry.entry_id] = coordinator
+    
+    _LOGGER.warning("NURTURE PLATFORMS LOADING: %s", PLATFORMS)
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+) -> bool:
+
+    unloaded = await hass.config_entries.async_unload_platforms(
+        entry,
+        PLATFORMS,
+    )
+
+    if unloaded:
+        hass.data[DOMAIN].pop(
+            entry.entry_id
+        )
+
+    return unloaded
